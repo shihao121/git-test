92e9fd3f7c0fc476d9f76ccfeb692c1af5b4106b
jijiu <2040736994@qq.com>
2019-07-29 15:55:42 +0800

    第一次提交项目

---
+++ src/edu/xupt/Main.java
@@ -0,0 +1,15 @@
+package edu.xupt;
+
+/**
+ * @CompanyL: 西安邮电大学
+ * @ClassName:
+ * @Description:
+ * @Author: 1569212975@qq.com
+ * @createDate: 2019/7/29 15:52
+ * @version: 0.0.1
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("测试git使用的主函数");
+    }
+}
